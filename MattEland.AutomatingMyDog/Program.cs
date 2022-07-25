2ea1e6703e027f9e7abb3fab5a0adf9d4f6b4566
Matt Eland <matt.eland@gmail.com>
2022-07-24 23:35:27 -0400

    Added TODOs for the project

--- MattEland.AutomatingMyDog/Program.cs
+++ MattEland.AutomatingMyDog/Program.cs
@@ -12,9 +12,16 @@ computerVision.Endpoint = data.Endpoint;
 
 string[] files = Directory.GetFiles("images");
 
+DemoImageAnalyzer analyzer = new();
 foreach (string imagePath in files)
 {
-    DemoImageAnalyzer analyzer = new();
     ImageAnalysis analysis = await analyzer.AnalyzeImageAsync(imagePath, computerVision);
 }
 
+// TODO: Speech synthesize a "Bark" if an animal is seen outdoors
+
+// TODO: Listen to a speech stream and transcribe it to words
+
+// TODO: Parse the speech file into an intent
+
+// TODO: Speech synthesize an "Arf" if the intent matches "speak"
