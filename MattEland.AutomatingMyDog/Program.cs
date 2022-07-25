63a4181c395c45a86ae3fd5b1a9a939393e1625b
Matt Eland <matt.eland@gmail.com>
2022-07-24 22:55:02 -0400

    Computer Vision now working, but clunky

--- MattEland.AutomatingMyDog/Program.cs
+++ MattEland.AutomatingMyDog/Program.cs
@@ -1,3 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+using MattEland.AutomatingMyDog;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+string key;
+string endpoint;
+
+using (StreamReader file = File.OpenText("credentials.json"))
+using (JsonTextReader reader = new(file))
+{
+    JObject jObj = (JObject)JToken.ReadFrom(reader);
+    key = jObj["key"].Value<string>();
+    endpoint = jObj["endpoint"].Value<string>();
+}
+
+ComputerVisionClient computerVision = new(new ApiKeyServiceClientCredentials(key));
+computerVision.Endpoint = endpoint;
+
+string imagePath = "images/DogCouchNap.jpg";
+
+if (!File.Exists(imagePath))
+{
+    Console.WriteLine("\nUnable to open or read local image path:\n{0} \n", imagePath);
+    return;
+}
+Console.WriteLine($"Analyzing {imagePath}...");
+
+IList<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>
+{
+    VisualFeatureTypes.Categories,
+    VisualFeatureTypes.Description,
+    VisualFeatureTypes.ImageType,
+    VisualFeatureTypes.Tags,
+    VisualFeatureTypes.Objects
+};
+
+ImageAnalysis analysis;
+using (Stream imageStream = File.OpenRead(imagePath))
+{
+    analysis = await computerVision.AnalyzeImageInStreamAsync(imageStream, features);
+}
+
+VisionConsoleHelper visionHelper = new();
+
+visionHelper.DisplayVisionCaptions(analysis);
+visionHelper.DisplayVisionObjects(analysis);
+visionHelper.DisplayVisionTags(analysis);
---
+++ MattEland.AutomatingMyDog/VisionConsoleHelper.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace MattEland.AutomatingMyDog;
+
+public class VisionConsoleHelper
+{
+
+    public void DisplayVisionCaptions(ImageAnalysis imageAnalysis)
+    {
+        Console.WriteLine("\r\nDescription: ");
+        foreach (ImageCaption? caption in imageAnalysis.Description.Captions)
+        {
+            if (caption == null) continue;
+
+            Console.WriteLine($"{caption.Text} Confidence: {caption.Confidence:p}");
+        }
+    }
+
+    public void DisplayVisionObjects(ImageAnalysis imageAnalysis)
+    {
+        Console.WriteLine("\r\nObjects:");
+        foreach (DetectedObject? obj in imageAnalysis.Objects)
+        {
+            if (obj == null) continue;
+
+            Console.WriteLine($"{obj.ObjectProperty} Confidence: {obj.Confidence:p} at ({obj.Rectangle.X},{obj.Rectangle.Y}):({obj.Rectangle.X + obj.Rectangle.W},{obj.Rectangle.Y + obj.Rectangle.H})");
+            ObjectHierarchy? parent = obj.Parent;
+            while (parent != null)
+            {
+                Console.WriteLine($"Parent: {parent.ObjectProperty} Confidence: {parent.Confidence:p}");
+                parent = parent.Parent;
+            }
+        }
+    }
+
+    public void DisplayVisionTags(ImageAnalysis imageAnalysis)
+    {
+        Console.WriteLine("\r\nTags:");
+        foreach (ImageTag? tag in imageAnalysis.Tags)
+        {
+            if (tag == null) continue;
+
+            Console.WriteLine($"{tag.Name} Confidence: {tag.Confidence:p}");
+        }
+    }
+
+}
