098ba46042a5db080f2445c38be97f704734e265
Matt Eland <matt.eland@gmail.com>
2022-07-26 10:19:12 -0400

    Speech to text is now functional.

--- MattEland.AutomatingMyDog/Program.cs
+++ MattEland.AutomatingMyDog/Program.cs
@@ -5,6 +5,9 @@ using Microsoft.CognitiveServices.Speech;
 ConfigurationManager configManager = new();
 ConfigData configData = configManager.LoadConfigData();
 
+SpeechConfig speechConfig = SpeechConfig.FromSubscription(configData.Key, configData.Region);
+speechConfig.SpeechSynthesisVoiceName = "en-US-GuyNeural";
+
 await ShowMainMenuAsync();
 
 async Task ShowMainMenuAsync()
@@ -19,6 +22,7 @@ async Task ShowMainMenuAsync()
         Console.WriteLine();
         Console.WriteLine("1) Look at pictures (and bark if anything is interesting)");
         Console.WriteLine("2) Listen to commands (and probably ignore them)");
+        Console.WriteLine("3) React to typed commands (and probably ignore them)");
         Console.WriteLine("Q) Quit");
         Console.WriteLine();
         string option = Console.ReadLine()!;
@@ -34,13 +38,17 @@ async Task ShowMainMenuAsync()
                 await ListenToSpeechAsync();
                 break;
             
+            case "3": // LUIS
+                await PromptForCommandAsync();
+                break;
+            
             case "Q": // Quit
                 stillGoing = false;
                 await SayMessageAsync("Goodbye, friend!");
                 break;
 
             default:
-                await SayMessageAsync("I don't understand.");
+                SayMessageAsync("I don't understand.");
                 break;
         }
     } while (stillGoing);
@@ -76,17 +84,55 @@ async Task LookAtPicturesAsync()
         {
             string message = $"I saw a {barkTarget}; Bark, bark, bark!";
 
-            await SayMessageAsync(message);
+            SayMessageAsync(message);
         }
     }
 }
 
 async Task ListenToSpeechAsync()
 {
+    // Listen to a speech stream and transcribe it to words
+    using (SpeechRecognizer recognizer = new(speechConfig))
+    {
+        SpeechRecognitionResult? result = await recognizer.RecognizeOnceAsync();
+
+        switch (result.Reason)
+        {
+            case ResultReason.Canceled:
+                Console.WriteLine("Speech Recognition canceled.");
+                break;
+            
+            case ResultReason.NoMatch:
+                Console.WriteLine("Speech Recognition could not understand audio.");
+                break;
+
+            case ResultReason.RecognizedSpeech:
+                await ProcessCommandAsync(result.Text);
+                break;
+        }
+    }
+}
+
+async Task PromptForCommandAsync()
+{
     // TODO: Listen to a speech stream and transcribe it to words
+    Console.WriteLine("What would you like to tell me?");
+    string input = Console.ReadLine()!;
+
+    await ProcessCommandAsync(input);
+}
+
+async Task ProcessCommandAsync(string command)
+{
+    // Protect against empty inputs; don't display any errors to the user, though
+    if (string.IsNullOrWhiteSpace(command))
+    {
+        return;
+    }
 
     // TODO: Display the words for validation
-    
+    Console.WriteLine($"You said: '{command}'");
+
     // TODO: Call out to text analytics and try to understand the parts of speech
 
     // TODO: Parse the text into an intent
@@ -97,9 +143,6 @@ async Task ListenToSpeechAsync()
 
 async Task SayMessageAsync(string message)
 {
-    SpeechConfig speechConfig = SpeechConfig.FromSubscription(configData.Key, configData.Region);
-    speechConfig.SpeechSynthesisVoiceName = "en-US-GuyNeural";
-    
     using SpeechSynthesizer synthesizer = new(speechConfig);
     using SpeechSynthesisResult? result = await synthesizer.SpeakTextAsync(message);
 }
