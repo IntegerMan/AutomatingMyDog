1dfc606fc7599ac740e8402c7f75d137046a8159
Matt Eland <matt.eland@gmail.com>
2022-09-06 00:04:42 -0400

    Reworked app to use new text analytics API. Expanded demos

--- MattEland.AutomatingMyDog/DemoTextAnalytics.cs
+++ MattEland.AutomatingMyDog/DemoTextAnalytics.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
-using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
-using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
 
 namespace MattEland.AutomatingMyDog;
 
@@ -10,8 +10,7 @@ public class DemoTextAnalytics
 
     public DemoTextAnalytics(string key, string endpoint)
     {
-        _textClient = new TextAnalyticsClient(new ApiKeyServiceClientCredentials(key));
-        _textClient.Endpoint = endpoint;
+        _textClient = new TextAnalyticsClient(new Uri(endpoint), new AzureKeyCredential(key));
     }
     
     public void AnalyzeText(string text)
@@ -19,50 +18,96 @@ public class DemoTextAnalytics
         Console.WriteLine("Using Text Analytics to analyze text...");
 
         // Detect Language
-        LanguageResult? langResult = _textClient.DetectLanguageAsync(text, countryHint: "US").Result;
-        string languageCode = "en";
-        if (langResult != null && langResult.DetectedLanguages.Any())
-        {
-            Console.WriteLine();
-            Console.WriteLine("Detected Language");
-            foreach (DetectedLanguage lang in langResult.DetectedLanguages)
-            {
-                Console.WriteLine($"{lang.Name}: {lang.Score:P}");
-            }
+        Response<DetectedLanguage> response = _textClient.DetectLanguage(text);
 
-            languageCode = langResult.DetectedLanguages.First().Iso6391Name;
-        }
+        DetectedLanguage lang = response.Value;
+        
+        Console.WriteLine($"{lang.Name} (ISO Code: {lang.Iso6391Name}): {lang.ConfidenceScore:P}");
 
         // Detect Key Phrases
-        KeyPhraseResult result = _textClient.KeyPhrasesAsync(text, language: languageCode).Result;
-        if (result.KeyPhrases.Any())
+        Response<KeyPhraseCollection> keyPhrasesResponse = _textClient.ExtractKeyPhrases(text);
+        KeyPhraseCollection keyPhrases = keyPhrasesResponse.Value;
+
+        Console.WriteLine();
+        Console.WriteLine("Key Phrases:");
+        foreach (string phrase in keyPhrases)
         {
-            Console.WriteLine();
-            Console.WriteLine("Key Phrases:");
-            foreach (string phrase in result.KeyPhrases)
-            {
-                Console.WriteLine($"\t{phrase}");
-            }
+            Console.WriteLine($"\t{phrase}");
         }
 
         // Detect Entities
-        EntitiesResult entities = _textClient.EntitiesAsync(text, language: languageCode).Result;
-        if (entities.Entities.Any())
+        Response<CategorizedEntityCollection> recognizeResponse = _textClient.RecognizeEntities(text);
+
+        Console.WriteLine();
+        Console.WriteLine("Entities:");
+
+        CategorizedEntityCollection entities = recognizeResponse.Value;
+        foreach (CategorizedEntity entity in entities)
         {
-            Console.WriteLine();
-            Console.WriteLine("Entities:");
-            foreach (EntityRecord entity in entities.Entities)
+            Console.WriteLine($"\t{entity.Text} (Category: {entity.Category}) with {entity.ConfidenceScore:P} confidence");
+        }
+
+        // Detect Linked Entities
+        Response<LinkedEntityCollection> linkedResponse = _textClient.RecognizeLinkedEntities(text);
+
+        Console.WriteLine();
+        Console.WriteLine("Linked Entities:");
+
+        // Loop over the linked entities recognized
+        LinkedEntityCollection linkedEntities = linkedResponse.Value;
+        foreach (LinkedEntity entity in linkedEntities)
+        {
+            Console.WriteLine($"\t{entity.Name} (Url: {entity.Url})");
+
+            // Each linked entity may have multiple matches
+            foreach (LinkedEntityMatch match in entity.Matches)
             {
-                Console.WriteLine($"\t{entity.Name}");
+                Console.WriteLine($"\t\tMatched on '{match.Text}' with {match.ConfidenceScore:P} confidence");
             }
         }
 
-        // Detect Sentiment
-        SentimentResult sentimentResult = _textClient.SentimentAsync(text, language: languageCode).Result;
-        if (sentimentResult.Score.HasValue)
+        // Detect PII
+        Response<PiiEntityCollection> piiResponse = _textClient.RecognizePiiEntities(text);
+        PiiEntityCollection piiEntities = piiResponse.Value;
+
+        Console.WriteLine();
+        Console.WriteLine("Redacted Text: " + piiEntities.RedactedText);
+        Console.WriteLine("PII Entities:");
+        
+        foreach (PiiEntity entity in piiEntities)
         {
-            Console.WriteLine();
-            Console.WriteLine($"Sentiment: {sentimentResult.Score}");
+            string category = entity.Category.ToString();
+            if (!string.IsNullOrWhiteSpace(entity.SubCategory))
+            {
+                category += "/" + entity.SubCategory;
+            }
+
+            Console.WriteLine($"\t{entity.Text} (Category: {category}) with {entity.ConfidenceScore:P} confidence");
+        }
+
+        // Detect Sentiment
+        Response<DocumentSentiment> sentimentResponse = _textClient.AnalyzeSentiment(text);
+        DocumentSentiment sentiment = sentimentResponse.Value;
+        
+        Console.WriteLine();
+        Console.WriteLine($"Overall Sentiment: {sentiment.Sentiment}");
+
+        // Log confidence scores for the overall sentiment
+        Console.WriteLine($"\tPositive: {sentiment.ConfidenceScores.Positive:P}");
+        Console.WriteLine($"\tNeutral: {sentiment.ConfidenceScores.Neutral:P}");
+        Console.WriteLine($"\tNegative: {sentiment.ConfidenceScores.Negative:P}");
+
+        Console.WriteLine();
+
+        // Log scores for each sentence
+        foreach (SentenceSentiment sentence in sentiment.Sentences)
+        {
+            Console.WriteLine($"'{sentence.Text}' had a sentiment of {sentence.Sentiment}");
+
+            // Log confidence scores for the sentence
+            Console.WriteLine($"\tPositive: {sentence.ConfidenceScores.Positive:P}");
+            Console.WriteLine($"\tNeutral: {sentence.ConfidenceScores.Neutral:P}");
+            Console.WriteLine($"\tNegative: {sentence.ConfidenceScores.Negative:P}");
         }
 
         InputHelper.PressAnyKey();
