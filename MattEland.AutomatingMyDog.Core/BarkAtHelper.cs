efbc1abd8cccf38cb71c3c60984f89e94a3fa56d
Matt Eland <matt.eland@gmail.com>
2023-05-28 21:49:13 -0400

    Better barking

--- MattEland.AutomatingMyDog.Core/BarkAtHelper.cs
+++ MattEland.AutomatingMyDog.Core/BarkAtHelper.cs
@@ -1,22 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace MattEland.AutomatingMyDog.Core;
 
-namespace MattEland.AutomatingMyDog.Core
+public static class BarkAtHelper
 {
-    public static class BarkAtHelper
+    public static bool IsSomethingToBarkAt(this string? thing)
     {
-        public static bool IsSomethingToBarkAt(this string? thing)
-        {
-            // Protect against null and mixed casing
-            thing = thing?.ToLowerInvariant() ?? "";
+        // Protect against null and mixed casing
+        thing = thing?.ToLowerInvariant() ?? "";
 
-            return thing.Contains("squirrel") ||
-                   thing.Contains("rabbit") ||
-                   thing.Contains("rodent") ||
-                   thing.Contains("dog");
-        }
+        return thing.Contains("squirrel") ||
+               thing.Contains("rabbit") ||
+               thing.Contains("rodent") ||
+               thing.Contains("toy") ||
+               thing.Contains("stuffed toy") ||
+               thing.Contains("plush") ||
+               thing.Contains("bear") ||
+               thing.Contains("raccoon") ||
+               thing.Contains("possum") ||
+               thing.Contains("cat") ||
+               thing.Contains("animal") ||
+               thing.Contains("bird") ||
+               thing.Contains("dog");
     }
 }
