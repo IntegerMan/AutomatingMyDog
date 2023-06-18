df064af9b3c876d972ffec9e05bc94a4c94527ff
Matt Eland <matt.eland@gmail.com>
2023-06-18 16:39:40 -0400

    LUIS Sample

--- MattEland.AutomatingMyDog.Desktop/ExampleCode/LUIS.cs
+++ MattEland.AutomatingMyDog.Desktop/ExampleCode/LUIS.cs
@@ -1,28 +1,50 @@
-﻿using System.Threading.Tasks;
-using Microsoft.CognitiveServices.Speech;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime.Models;
 
-public async Task<bool> SayHelloAsync() {
+public async Task<string> MatchUtteranceToIntentAsync(string utterance) {
 
-    // Read these from a config file in production
-    string region = "northcentralus";
+    // LUIS Settings - again, store in a config file
     string subscriptionKey = "123abc45def67g89h0i12345jk6lmno7";
+    string slotId = "Production"; // or "Staging"
+    Guid appId = new Guid("3d8c9e1b-7c5f-4e1a-9f3d-6f5a7b2a6d5e"); // Found in Azure
 
-    // Configure your subscription and region
-    SpeechConfig config = SpeechConfig.FromSubscription(subscriptionKey, region);
+    // Authenticate with Azure
+    ApiKeyServiceClientCredentials creds = new(subscriptionKey);
+    LUISRuntimeClient client = new(creds);
 
-    // Set up the Synthesizer to use the voice we want
-    config.SpeechSynthesisVoiceName = "en-US-GuyNeural";
-    using SpeechSynthesizer synthesizer = new(config);
+    // Call out to LUIS to get a prediction
+    PredictionRequest request = new(message);
+    IPredictionOperations predictions = client.Prediction;
+    PredictionResponse predictResult =
+        await predictions.GetSlotPredictionAsync(appId, slotId, request, showAllIntents: true);
 
-    // Actually say the message
-    string message = "Welcome to DogOS";
-    using SpeechSynthesisResult result = await synthesizer.SpeakTextAsync(message);
 
-    // If we're correctly configured this should give us a completed result
-    bool succeeded = result.Reason == ResultReason.SynthesizingAudioCompleted;
+    // Get the top intent
+    Prediction prediction = predictResult.Prediction;
+    string topIntent = prediction.TopIntent;
 
-    // result.AudioData will have the raw WAV file if you want to save it
-    byte[] wavData = result.AudioData;
 
-    return succeeded;
+
+    // List all considered intents and their confidence %
+    foreach (KeyValuePair<string, Intent> intent in prediction.Intents) {
+        Console.WriteLine($"{intent.Key}: {intent.Value.Score}");
+    }
+}
+
+
+
+
+public string GetMessageFromMatchedIntent(string intent) {
+    switch (intent.ToUpperInvariant()) {
+        case "GOOD BOY":
+            return "Jester is a good doggo!";
+
+        case "WALK":
+            return "Why yes, Jester DOES want to go on a walk!";
+
+        default:
+            return "Jester does not understand.";
+    }
 }
--- MattEland.AutomatingMyDog.Desktop/ExampleCode/TextAnalysis.cs
+++ MattEland.AutomatingMyDog.Desktop/ExampleCode/TextAnalysis.cs
@@ -1,28 +1,85 @@
 ﻿using System.Threading.Tasks;
-using Microsoft.CognitiveServices.Speech;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Azure.AI.TextAnalytics;
+using Azure;
 
-public async Task<bool> SayHelloAsync() {
+public async Task<IEnumerable<string>> AnalyzeTextAsync(string text) {
 
-    // Read these from a config file in production
-    string region = "northcentralus";
+    // Configuration settings - store these in a config file
     string subscriptionKey = "123abc45def67g89h0i12345jk6lmno7";
+    string endpoint = "https://yourendpoint.cognitiveservices.azure.com/";
+    Uri endpointUri = new(endpoint);
 
-    // Configure your subscription and region
-    SpeechConfig config = SpeechConfig.FromSubscription(subscriptionKey, region);
+    // Set up our TextAnalyticsClient
+    AzureKeyCredential azureCreds = new(subscriptionKey);
+    TextAnalyticsClient client = new(endpointUri, azureCreds);
 
-    // Set up the Synthesizer to use the voice we want
-    config.SpeechSynthesisVoiceName = "en-US-GuyNeural";
-    using SpeechSynthesizer synthesizer = new(config);
 
-    // Actually say the message
-    string message = "Welcome to DogOS";
-    using SpeechSynthesisResult result = await synthesizer.SpeakTextAsync(message);
 
-    // If we're correctly configured this should give us a completed result
-    bool succeeded = result.Reason == ResultReason.SynthesizingAudioCompleted;
+    // Detect Language
+    Response<DetectedLanguage> langResponse = client.DetectLanguage(text);
+    DetectedLanguage language = langResponse.Value;
+    yield return $"Detected Language {language.Name} ({langResponse.Value.Iso6391Name})" +
+        $" with confidence of {langResponse.Value.ConfidenceScore:P0}";
 
-    // result.AudioData will have the raw WAV file if you want to save it
-    byte[] wavData = result.AudioData;
 
-    return succeeded;
+
+    // Detect Key Phrases
+    Response<KeyPhraseCollection> keyPhrasesResponse = client.ExtractKeyPhrases(text);
+    KeyPhraseCollection keyPhrases = keyPhrasesResponse.Value;
+    yield return $"Key Phrases: {string.Join(", ", keyPhrases)}";
+
+
+
+    // Detect Entities
+    Response<CategorizedEntityCollection> recognizeResponse = client.RecognizeEntities(text);
+    CategorizedEntityCollection entities = recognizeResponse.Value;
+    foreach (CategorizedEntity entity in entities) {
+        yield return $"Detected entity '{entity.Text}' " +
+            $"(Category: {entity.Category}) with " +
+            $"{entity.ConfidenceScore:P0} confidence";
+    }
+
+
+
+    // Detect Linked Entities
+    Response<LinkedEntityCollection> linkedResponse = client.RecognizeLinkedEntities(text);
+    LinkedEntityCollection linkedEntities = linkedResponse.Value;
+    foreach (LinkedEntity entity in linkedEntities) {
+        yield return $"Linked Entity detected: '{entity.Name}' " +
+            $"with Url: {entity.Url}";
+    }
+
+
+
+    // Detect PII
+    Response<PiiEntityCollection> piiResponse = client.RecognizePiiEntities(text);
+    PiiEntityCollection piiEntities = piiResponse.Value;
+    foreach (PiiEntity entity in piiEntities) {
+        string category = entity.Category.ToString();
+        if (!string.IsNullOrWhiteSpace(entity.SubCategory)) {
+            category += "/" + entity.SubCategory;
+        }
+
+        yield return $"PII Encountered '{entity.Text}' " +
+            $"(Category: {category}) with " +
+            $"{entity.ConfidenceScore:P0} confidence";
+    }
+    yield return $"Redacted Text: {piiEntities.RedactedText}";
+
+
+
+    // Detect Sentiment
+    Response<DocumentSentiment> sentimentResponse = client.AnalyzeSentiment(text);
+    DocumentSentiment sentiment = sentimentResponse.Value;
+    SentimentConfidenceScores confidence = sentiment.ConfidenceScores;
+    yield return $"Detected Sentiment {sentiment.Sentiment} with " +
+        $"positive / neutral / negative confidence scores of " +
+        $"{sentiment.ConfidenceScores.Positive:P0} / " +
+        $"{confidence.Neutral:P0} / " +
+        $"{sentiment.ConfidenceScores.Negative:P0}";
+
+
 }
